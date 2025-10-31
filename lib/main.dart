3ada269509683898f04de72082aad504c863baa9
abday_hafidz <abday.hafidz23@gmail.com>
2025-10-31 11:27:40 +0700

    ngerjain main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'screen/splash_screen_dart1.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-  
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NIM : 1123150093, Muhammad abday abdul hafidz',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 28, 2, 225)
+        ),
+      ),
+      home: SplashScreenDart1(),
+    );
   }
 }
---
+++ lib/screen/splash_screen_dart1.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class SplashScreenDart1 extends StatelessWidget {
+  const SplashScreenDart1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+            appBar: AppBar(
+              title: const Text("Dashboard"),
+              actions: const [],
+            ),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: const Column(
+                  children: [],
+                ),
+              ),
+            ),
+          );;
+  }
+}
